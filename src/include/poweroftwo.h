791e33e4b5306741ad2257c4c157fd2a547d8e91
humasama <pandahjy110@163.com>
2014-05-31 17:10:52 +0800

    latest poweroftwo.h

--- src/include/poweroftwo.h
+++ src/include/poweroftwo.h
@@ -5,14 +5,17 @@
 #include "parallel.h"
 
 #if defined(POWEROFTWO)
-#if defined(HIPAC_ESTABLISHED_POS)
-void delete_tcb(char *data);
+
+#if defined(HIPAC_TCB)
+void delete_tcp(char *data);
 #endif
+
 struct tcp_stream *find_stream(struct tcphdr *, struct ip *, int *);
-void add_new_tcp(struct tcphdr *, struct ip *);
+static idx_type add_new_tcp(struct tcphdr *, struct ip *);
 void nids_free_tcp_stream(struct tcp_stream *);
 int process_tcp(u_char *, int);
 int tcp_init(int);
 void tcp_exit();
 #endif
+
 #endif
